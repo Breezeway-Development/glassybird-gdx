aa29b9bc716407874993287fc8931957eae387d8
Cove Geary <cogeary01@gmail.com>
2014-04-21 11:08:42 -0500

    Init

---
+++ html/src/com/breezewaydevelopment/glassybird/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.breezewaydevelopment.glassybird.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.breezewaydevelopment.glassybird.glassybird;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new glassybird();
+        }
+}
