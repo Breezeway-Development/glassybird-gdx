773fe734d8b5d0edb23a8e249f997bd07aa9a1f8
Cove Geary <cogeary01@gmail.com>
2014-04-21 18:50:10 -0500

    Day 6 - Working on textures

--- core/src/com/breezewaydevelopment/helpers/AssetLoader.java
+++ core/src/com/breezewaydevelopment/helpers/AssetLoader.java
@@ -1,5 +1,58 @@
 package com.breezewaydevelopment.helpers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class AssetLoader {
+
+    public static Texture texture;
+    public static TextureRegion bg, grass;
+
+    public static Animation birdAnimation;
+    public static TextureRegion bird, birdDown, birdUp;
+
+    public static TextureRegion skullUp, skullDown, bar;
+
+    public static void load() {
+
+        texture = new Texture(Gdx.files.internal("data/texture.png")); // Texture is like a spritesheet
+        texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest); // Minification and magnification
+
+        bg = new TextureRegion(texture, 0, 0, 136, 43); // x, y, width, height
+        bg.flip(false, true); // flip x, y. libGDX assumes a y-up coord system
+
+        grass = new TextureRegion(texture, 0, 43, 143, 11);
+        grass.flip(false, true);
+
+        birdDown = new TextureRegion(texture, 136, 0, 17, 12);
+        birdDown.flip(false, true);
+
+        bird = new TextureRegion(texture, 153, 0, 17, 12);
+        bird.flip(false, true);
+
+        birdUp = new TextureRegion(texture, 170, 0, 17, 12);
+        birdUp.flip(false, true);
+
+        TextureRegion[] birds = { birdDown, bird, birdUp };
+        birdAnimation = new Animation(0.06f, birds);
+        birdAnimation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+
+        skullUp = new TextureRegion(texture, 192, 0, 24, 14);
+        // Create by flipping existing skullUp
+        skullDown = new TextureRegion(skullUp);
+        skullDown.flip(false, true);
+
+        bar = new TextureRegion(texture, 136, 16, 22, 3);
+        bar.flip(false, true);
+
+    }
+    
+    public static void dispose() {
+        // We must dispose of the texture when we are finished.
+        texture.dispose();
+    }
 
 }
